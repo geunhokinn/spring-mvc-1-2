f6f20ae3431a8a4046d5aae32a39d16e1ee892bb
geunhokinn <gghh1220@gmail.com>
2024-04-29 21:53:03 +0900

    feat: http 응답 - 정적 리소스, 뷰 템플릿 추가

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello").
+                addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    } // 권장
+
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+    // 경로 이름이랑 반환이 같이면 반환 생략 가능. 불명확해서 권장하지 않음.
+}
