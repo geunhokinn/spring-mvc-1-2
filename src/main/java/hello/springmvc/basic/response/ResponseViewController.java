f0eb5130dc4f944db2d9cfa95cc612458e7f5609
geunhokinn <gghh1220@gmail.com>
2024-04-30 00:23:05 +0900

    feat: @Controller 추가

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.response;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class ResponseViewController {
 
     @RequestMapping("/response-view-v1")
